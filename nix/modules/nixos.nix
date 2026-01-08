a80b8672b851b415c32420f41a63fc2cdb29ca49
Steven Scott <codingismy11to7@gmail.com>
2026-01-08 03:24:04 -0500

    add hyprland configuration

---
+++ nix/modules/nixos.nix
@@ -0,0 +1,47 @@
+{ self, inputs }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.options) mkOption mkEnableOption mkPackageOption;
+  inherit (lib.trivial) importTOML;
+  inherit (lib.meta) getExe;
+  inherit (lib.types) nullOr bool;
+  inherit (lib)
+    optionalString
+    types
+    mapAttrs'
+    mapAttrsToList
+    nameValuePair
+    mkDefault
+    literalExpression
+    ;
+
+  cfg = config.omarchy;
+in
+{
+  options = {
+    omarchy = {
+      enable = mkEnableOption self.description;
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+
+    # quick previewer for nautilus
+    services.gnome.sushi.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      nautilus
+    ];
+  };
+}
