97d4a1863e90fae2fddddff3e4a7bae2d866dd82
Steven Scott <codingismy11to7@gmail.com>
2026-01-09 20:56:18 -0500

    fix theme preview

--- nix/modules/home/walker.nix
+++ nix/modules/home/walker.nix
@@ -15,11 +15,16 @@ lib.mkIf cfg.enable {
   };
 
   xdg = {
-    # the walker home-manager module enables elephant, so
-    # configure it here
-    configFile.elephant = {
-      source = ../../../config/elephant;
-      recursive = true;
+    configFile = {
+      # the walker home-manager module enables elephant, so
+      # configure it here
+      "elephant/calc.toml".source = ../../../config/elephant/calc.toml;
+      "elephant/desktopapplications.toml".source = ../../../config/elephant/desktopapplications.toml;
+      "elephant/menus/omarchy_themes.lua".source =
+        pkgs.replaceVars ../../../default/elephant/omarchy_themes.lua
+          { omarchyThemesDir = ../../../themes; };
+
+      "walker/config.toml".source = ../../../config/walker/config.toml;
     };
 
     dataFile."omarchy/default/walker/themes/omarchy-default/layout.xml".source =
@@ -28,9 +33,5 @@ lib.mkIf cfg.enable {
     dataFile."omarchy/default/walker/themes/omarchy-default/style.css".source =
       pkgs.replaceVars ../../../default/walker/themes/omarchy-default/style.css
         { styleImport = ../../../themes/${cfg.theme}/walker.css; };
-
-    configFile = {
-      "walker/config.toml".source = ../../../config/walker/config.toml;
-    };
   };
 }
