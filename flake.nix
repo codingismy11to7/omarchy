5d295da3b9e72166b3d33aabcd3a526b59872f79
Steven Scott <codingismy11to7@gmail.com>
2026-01-10 04:54:04 -0500

    add test build for faster feedback loop

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     terminaltexteffects = {
       # url = "github:ChrisBuilds/terminaltexteffects/release-0.14.2";
       url = "github:codingismy11to7/terminaltexteffects/fix_nix_build";
@@ -29,6 +33,7 @@
   outputs =
     {
       self,
+      home-manager,
       nixpkgs,
       systems,
       terminaltexteffects,
@@ -38,7 +43,8 @@
     let
       inherit (nixpkgs) lib;
 
-      forAllSystems = f: lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      eachSystem = lib.genAttrs (import systems);
+      forAllSystems = f: eachSystem (system: f nixpkgs.legacyPackages.${system});
     in
     {
       formatter = forAllSystems (pkgs: pkgs.nixfmt);
@@ -52,5 +58,56 @@
         default = self.nixosModules.omarchy;
         omarchy = import ./nix/modules/nixos.nix { inherit self inputs; };
       };
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          shellHook = ''
+            echo "*** watch for changes with 'dev-mode' ***"
+          '';
+          packages = with pkgs; [
+            watchexec
+            (pkgs.writeShellScriptBin "dev-mode" ''
+              watchexec --restart --clear --ignore result nix flake check
+            '')
+          ];
+        };
+      });
+
+      checks = eachSystem (system: {
+        test-build =
+          (lib.nixosSystem {
+            inherit system;
+            specialArgs = { inherit inputs; };
+            modules = [
+              self.nixosModules.default
+              home-manager.nixosModules.home-manager
+              {
+
+                fileSystems."/".device = "/dev/null";
+                boot.loader.grub.enable = false;
+                system.stateVersion = "25.11";
+                users.users.testuser = {
+                  isNormalUser = true;
+                  group = "testuser";
+                };
+                users.groups.testuser = { };
+                home-manager = {
+                  useUserPackages = true;
+                  useGlobalPkgs = true;
+                  extraSpecialArgs = { inherit inputs; };
+                  users.testuser = {
+                    imports = [
+                      self.homeManagerModules.default
+                    ];
+
+                    home.stateVersion = "25.11";
+                    omarchy.enable = true;
+                    omarchy.browser = "brave";
+                  };
+                };
+              }
+            ];
+          }).config.system.build.toplevel;
+      });
     };
 }
